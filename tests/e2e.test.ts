e77f445d4eb3b821ec687dc4604c108705aa6e94
M Pekacki <mpekacki@craftware.biz>
2024-10-23 12:10:37 +0200

    exec promise in test

--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -1,7 +1,14 @@
 import { test, expect } from '@jest/globals';
 import { Connection, AuthInfo } from '@salesforce/core';
+import util from 'util';
 import { exec } from 'child_process';
 import fs from 'fs';
+
+const execPromise = util.promisify(exec);
+
+afterEach(async () => {
+    fs.unlinkSync('./config.json');
+});
 
 test('migrate record', async () => {
     // Increase timeout to 30 seconds
@@ -88,66 +95,51 @@ test('migrate record', async () => {
     fs.writeFileSync('./config.json', JSON.stringify(config, null, 2));
 
     // when
-    // await main('testMigrationOrgA', 'testMigrationOrgB', opportunity.id!, onOutput);
-    await new Promise<void>((resolve, reject) => {
-        exec(`npx ts-node ./main.ts --config-json ./config.json`, async (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                reject(error);
-                return;
-            }
-            capturedOutput += stdout;
+    const { stdout, stderr } = await execPromise(`npx ts-node ./main.ts --config-json ./config.json`);
+    capturedOutput += stdout;
+    
+    // then
+    // should output old record ids to new record ids, e.g. {"006xx000001234AAA":"006yy000002345BBB","001xx000003456CCC":"001yy000004567DDD"}
+    const outputLines = capturedOutput.split('\n');
+    console.log('outputLines', outputLines);
+    expect(outputLines.length).toBeGreaterThan(1);
+    const parsedOutput = JSON.parse(outputLines[outputLines.length - 2]);
 
-            try {
-                // then
-                // should output old record ids to new record ids, e.g. {"006xx000001234AAA":"006yy000002345BBB","001xx000003456CCC":"001yy000004567DDD"}
-                const outputLines = capturedOutput.split('\n');
-                console.log('outputLines', outputLines);
-                expect(outputLines.length).toBeGreaterThan(1);
-                const parsedOutput = JSON.parse(outputLines[outputLines.length - 2]);
-            
-                // Check if opportunity was migrated
-                expect(parsedOutput).toHaveProperty(opportunity.id!);
-                const newOpportunityId = parsedOutput[opportunity.id!];
-                expect(newOpportunityId).toBeTruthy();
-                expect(newOpportunityId).not.toEqual(opportunity.id);
+    // Check if opportunity was migrated
+    expect(parsedOutput).toHaveProperty(opportunity.id!);
+    const newOpportunityId = parsedOutput[opportunity.id!];
+    expect(newOpportunityId).toBeTruthy();
+    expect(newOpportunityId).not.toEqual(opportunity.id);
 
-                // Check if account was migrated
-                expect(parsedOutput).toHaveProperty(account.id!);
-                const newAccountId = parsedOutput[account.id!];
-                expect(newAccountId).toBeTruthy();
-                expect(newAccountId).not.toEqual(account.id);
+    // Check if account was migrated
+    expect(parsedOutput).toHaveProperty(account.id!);
+    const newAccountId = parsedOutput[account.id!];
+    expect(newAccountId).toBeTruthy();
+    expect(newAccountId).not.toEqual(account.id);
 
-                // Check if contact was migrated
-                expect(parsedOutput).toHaveProperty(contact.id!);
-                const newContactId = parsedOutput[contact.id!];
-                expect(newContactId).toBeTruthy();
-                expect(newContactId).not.toEqual(contact.id);
+    // Check if contact was migrated
+    expect(parsedOutput).toHaveProperty(contact.id!);
+    const newContactId = parsedOutput[contact.id!];
+    expect(newContactId).toBeTruthy();
+    expect(newContactId).not.toEqual(contact.id);
 
-                // should be able to query the new opportunity record
-                const newOpportunity: any = await conn2.sobject('Opportunity').retrieve(newOpportunityId);
-                expect(newOpportunity).toBeDefined();
-                expect(newOpportunity.Name).toEqual('Blasto Bandage');
-                expect(newOpportunity.CampaignId).toEqual(campaignOrgB.id);
+    // should be able to query the new opportunity record
+    const newOpportunity: any = await conn2.sobject('Opportunity').retrieve(newOpportunityId);
+    expect(newOpportunity).toBeDefined();
+    expect(newOpportunity.Name).toEqual('Blasto Bandage');
+    expect(newOpportunity.CampaignId).toEqual(campaignOrgB.id);
 
-                // should be able to query the new account record
-                const newAccount: any = await conn2.sobject('Account').retrieve(newAccountId);
-                expect(newAccount).toBeDefined();
-                expect(newAccount.Name).toEqual('Ebola Cola');
+    // should be able to query the new account record
+    const newAccount: any = await conn2.sobject('Account').retrieve(newAccountId);
+    expect(newAccount).toBeDefined();
+    expect(newAccount.Name).toEqual('Ebola Cola');
 
-                // should be able to query the new contact record
-                const newContact: any = await conn2.sobject('Contact').retrieve(newContactId);
-                expect(newContact).toBeDefined();
-                expect(newContact.FirstName).toEqual('Spider');
-                expect(newContact.LastName).toEqual('Jerusalem');
+    // should be able to query the new contact record
+    const newContact: any = await conn2.sobject('Contact').retrieve(newContactId);
+    expect(newContact).toBeDefined();
+    expect(newContact.FirstName).toEqual('Spider');
+    expect(newContact.LastName).toEqual('Jerusalem');
 
-                // Check if the new opportunity is associated with the new account
-                expect(newOpportunity.AccountId).toEqual(newAccountId);
-
-                resolve();
-            } catch (error) {
-                reject(error);
-            }
-        });
-    });
+    // Check if the new opportunity is associated with the new account
+    expect(newOpportunity.AccountId).toEqual(newAccountId);
 }, 30000);
