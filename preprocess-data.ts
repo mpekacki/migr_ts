0530e0cc8bc9bb96be30e192d56c15ecadc655ef
M Pekacki <mpekacki@craftware.biz>
2025-06-03 10:05:37 +0200

    very simple email anonymization

--- preprocess-data.ts
+++ preprocess-data.ts
@@ -6,6 +6,14 @@ type PreprocessStrategy = {
 
 export const preprocessData = (recordsByIds: Record<string, SObjectRecord<Schema, string>>, strategy: PreprocessStrategy) => {
     if (strategy.anonymizeEmailFields) {
-        
+        for (const recordId in recordsByIds) {
+            const record = recordsByIds[recordId];
+            for (const field in record) {
+                // if value contains @, clear it
+                if (record[field] && record[field].includes('@')) {
+                    record[field] = '';
+                }
+            }
+        }
     }
 }
--- tests/preprocess-data..test.ts
+++ tests/preprocess-data..test.ts
@@ -4,22 +4,26 @@ describe('preprocessData', () => {
     it('should not modify records if no strategy is provided', () => {
         const recordsByIds = {
             '0012x0000000000': {
+                name: 'John Smith',
                 email: 'test@example.com',
             },
         };
-        preprocessData(recordsByIds);
+        preprocessData(recordsByIds, {});
+        expect(recordsByIds['0012x0000000000'].name).toBe('John Smith');
         expect(recordsByIds['0012x0000000000'].email).toBe('test@example.com');
     });
 
     it('should anonymize email fields', () => {
         const recordsByIds = {
             '0012x0000000000': {
+                name: 'John Smith',
                 email: 'test@example.com',
             },
         };
         preprocessData(recordsByIds, {
             anonymizeEmailFields: true,
         });
+        expect(recordsByIds['0012x0000000000'].name).toBe('John Smith');
         expect(recordsByIds['0012x0000000000'].email).not.toBe('test@example.com');
     });
 });
