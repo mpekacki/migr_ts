28aa34381e57b06edec2acd174285a2c40fdefb8
M Pekacki <mpekacki@craftware.biz>
2025-08-30 16:09:02 +0200

    better test output

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  // Better output for CI/CD
+  verbose: true,
+  reporters: [
+    'default'
+  ],
+  // Show individual test results as they complete
+  testTimeout: 120000, // 2 minutes per test (for e2e tests)
 };
